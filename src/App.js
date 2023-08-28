19892a5f0477cf887ef769593368a13d2adc8c29
Deepak Kukrety <dkukrety@gmail.com>
2023-08-28 16:54:24 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import UsersComponent from "./Components/UsersComponent";
 
 function App() {
   return (
     <>
-    
-    
+      <UsersComponent />
     </>
   );
 }
--- src/Components/UsersComponent.js
+++ src/Components/UsersComponent.js
@@ -1,10 +1,23 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+
+const baseURL = "https://jsonplaceholder.typicode.com/users";
 
 const UsersComponent = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  if (!users) return null;
+
   return (
     <div>
-      
-      <h2>User Component</h2>
+      <h1>{users.name}</h1>
+      <p>{users.email}</p>
     </div>
   );
 };
