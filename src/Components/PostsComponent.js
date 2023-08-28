d635ac6dbce725bc5a1531f40443aea98d26860f
ManojGowda701 <manojgowda701@gmail.com>
2023-08-28 18:49:36 +0530

    pagination failed

--- src/Components/PostsComponent.js
+++ src/Components/PostsComponent.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
-// import './Posts.css'
+import { Button, Card, CardBody, CardTitle, CardSubtitle, CardText, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
 
 function PostsComponent() {
     const [posts, setPosts] = useState([])
 
     const [isLoading, setIsLoading] = useState(false);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 12;
 
   useEffect(() => {
     if (isLoading) {
@@ -26,6 +29,10 @@ function PostsComponent() {
     setIsLoading(true);
   };
 
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
   return (
     <>
         <div className='container'>
@@ -35,26 +42,50 @@ function PostsComponent() {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-
-        <div className='d-flex flex-row flex-wrap '>
+        <>
+               <div className='d-flex flex-row flex-wrap '>
           {posts.map((post, index) => (
-          
-            <Card key = {index} className='w-25'>
-              <CardBody>
-                <CardTitle tag="h5"><li key={post.title}>{post.title}</li></CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6"><li key={post.id}>{post.id}</li></CardSubtitle>
-              </CardBody>
-              <img alt="Card cap" src="https://picsum.photos/318/180" width="100%"/>
-              <CardBody>
-                <CardText><li key={post.body}>{post.body}</li></CardText>
-              </CardBody>
-            </Card>
-              
-              
+            
+                 <Card key = {index} className='w-25'>
+                  <CardBody>
+                    <CardTitle tag="h5"><li key={post.title}>{post.title}</li></CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6"><li key={post.id}>{post.id}</li></CardSubtitle>
+                  </CardBody>
+                      <img alt="Card cap" src="https://picsum.photos/318/180" width="100%"/>
+                  <CardBody>
+                    <CardText><li key={post.body}>{post.body}</li></CardText>
+                  </CardBody>
+                </Card>     
           ))}
         </div>
+
+        <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={posts.length < postsPerPage}
+        >
+          Next
+        </button>
+      </div>
+        </>
+       
+        
       )}
     </div>
+    <div className="pagination">
+        {Array.from({ length: Math.ceil(posts.length / postsPerPage) }, (_, index) => (
+          <button key={index} className={index + 1 === currentPage ? 'active' : ''} onClick={() => setCurrentPage(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </>
   );
 }
