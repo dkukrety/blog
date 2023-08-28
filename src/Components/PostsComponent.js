51a3b30c6d06495b63d760fcd58206c1cbdd72ed
ManojGowda701 <manojgowda701@gmail.com>
2023-08-28 16:33:52 +0530

    Posts added

--- src/Components/PostsComponent.js
+++ src/Components/PostsComponent.js
@@ -1,13 +1,54 @@
-import { React, useState } from "react";
+import React, { useState, useEffect } from 'react'
+import Post from './Post'
+import { Button } from 'reactstrap';
 
-const PostsComponent = () => {
-  const [posts, setPosts] = useState([]);
+function PostsComponent() {
+    const [posts, setPosts] = useState([])
+
+    const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      fetch('https://jsonplaceholder.typicode.com/posts') // Replace with your API URL
+        .then(response => response.json())
+        .then(posts => {
+            setPosts(posts);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          setIsLoading(false);
+        });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => {
+    setIsLoading(true);
+  };
 
   return (
-    <div>
-      <h2>Post Component</h2>
+    <>
+        <div>
+            <div>
+                <Button color="info" onClick={handleClick}>Posts</Button>
+            </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {posts.map(post => (
+            <>
+            <li key={post.userId}>{post.userId}</li>
+            <li key={post.id}>{post.id}</li>
+            <li key={post.title}>{post.title}</li>
+            <li key={post.body}>{post.body}</li>
+            </>
+          ))}
+        </ul>
+      )}
     </div>
+    </>
   );
-};
+}
 
-export default PostsComponent;
+export default PostsComponent
